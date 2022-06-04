263dec086e595560df1e9c6734b1208cd3d96a6d
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2022-06-04 14:59:58 +0530

    Add files via upload

---
+++ Lab1/sample.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+/*Java Versions :
+Java is a class based, object oriented programming language. It was developed in 1991 at Sun Microsystems.
+Till now, Java has many versions and by each new version the efficiency and compatibility is appreciable.
+
+Java JDK and JRE :
+JDK is the short form of Java Development Kit. It is used to execute java programs.
+JRE is the short form for Java Runtime Environment. It provides essential resources and class libraries that are needed to run a program in Java
+*/
+
+//Hello world program
+
+class sample {
+    public static void main( String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
